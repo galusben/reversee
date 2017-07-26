351106bc23ce89f3a1ad51608796f5ffbcf5f505
galba <galba@jfrog.com>
2017-07-26 16:30:55 +0300

    uglify

---
+++ afterCopy.js
@@ -0,0 +1,32 @@
+const uglifyJS = require("uglify-es");
+const fs = require("fs");
+const path = require("path");
+console.log("Loading custom after copy");
+
+module.exports = function (buildPath, electronVersion, platform, arch, callback) {
+    console.log("Running custom after copy");
+    console.log(buildPath);
+    console.log(electronVersion);
+    console.log(platform);
+    console.log(arch);
+    const sourcePath = path.join(buildPath, 'src')
+    const items = fs.readdirSync(sourcePath);
+
+    console.log(items);
+    for (let i = 0; i < items.length; i++) {
+        console.log(items[i]);
+        const filePath = path.join(sourcePath, items[i]);
+        if (fs.lstatSync(filePath).isFile() && filePath.endsWith('.js')) {
+            const contentBeforeMinify = fs.readFileSync(filePath);
+            console.log('Content:' + contentBeforeMinify);
+            const mini = uglifyJS.minify(contentBeforeMinify, { warnings: true, mangle: {toplevel: true} });
+            if (mini.error) {
+                return callback(mini.error);
+            }
+            fs.writeFileSync(filePath, mini.code)
+        }
+    }
+
+
+    return callback();
+};
