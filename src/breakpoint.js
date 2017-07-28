1cac1cf05bce1aa330747d9b0714d5ed48644d40
galba <galba@jfrog.com>
2017-07-28 11:08:58 +0300

    minor code fixes

--- src/breakpoint.js
+++ src/breakpoint.js
@@ -1,13 +1,13 @@
 const {ipcRenderer} = require('electron');
 
-var breakPoint;
+let breakPoint;
 
 function continu() {
-    var headers = {};
+    let headers = {};
 
     $('#headers-table').children('tbody').children('tr').each(function (i, tr) {
-        var headerName = $($(tr).children('td')[1]).text();
-        var headerValue = $($(tr).children('td')[2]).text();
+        let headerName = $($(tr).children('td')[1]).text();
+        let headerValue = $($(tr).children('td')[2]).text();
         if (headerName) {
             headers[headerName] = headerValue;
         }
@@ -32,8 +32,8 @@ function addRow() {
 ipcRenderer.on('breaking', (event, arg) => {
     $('#url').val(arg.url);
     $('#method').val(arg.method);
-    var tbody = $('#headers-table').children('tbody');
-    var headers = arg.headers;
+    let tbody = $('#headers-table').children('tbody');
+    let headers = arg.headers;
     for (let key in headers) {
         tbody.append(
             $(`<tr><td></td><td contenteditable="true">${key}</td><td contenteditable="true">${headers[key]}</td></tr>`)
